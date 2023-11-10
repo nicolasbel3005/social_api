3979ac6d283c7675500c1d2d83362550be71f155
Nicolas Esquibel <n.m.esquibel01@gmail.com>
2023-11-09 22:43:38 -0800

    updates

--- apps.js
+++ apps.js
@@ -1,8 +1,11 @@
+// app.js
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user'); // Adjust the path as needed
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const User = require('./models/user');
+const jwtSecret = 'your-jwt-secret-key'; // Change this to a secure secret key
 
 const app = express();
 
@@ -24,19 +27,32 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Configure passport-jwt strategy
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtSecret,
+    },
+    (jwtPayload, done) => {
+      User.findById(jwtPayload.sub)
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        })
+        .catch((err) => done(err, false));
+    }
+  )
+);
+
 // ... (Your other middleware and routes)
 
-// Example of a protected route
-app.get('/protected-route', ensureAuthenticated, (req, res) => {
+// Example of a protected route using JWT
+app.get('/protected-route', passport.authenticate('jwt', { session: false }), (req, res) => {
   res.json({ message: 'This route is protected.' });
 });
-
-// Middleware to check if the user is authenticated
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.status(401).json({ message: 'Unauthorized' });
-}
 
 // ... (Your other routes and server setup)
