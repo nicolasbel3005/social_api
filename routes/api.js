4511c5c7f0c26676ab6d4328232d556f59480c54
Nicolas Esquibel <n.m.esquibel01@gmail.com>
2023-11-09 22:59:49 -0800

    updates

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,20 @@
+// routes/api.js
 const express = require('express');
 const router = express.Router();
-const { validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const { User, Thought, Reaction } = require('../models');
 
 // ... (Other user routes)
 
-// POST to create a new thought with input validation
+// POST route for user registration
 router.post(
-  '/thoughts',
+  '/register',
   [
-    // Input validation
-    check('thoughtText').isLength({ min: 1, max: 280 }).withMessage('Thought must be 1-280 characters'),
     check('username').trim().notEmpty().withMessage('Username is required'),
-    check('userId').isMongoId().withMessage('Invalid user ID'),
+    check('email').isEmail().normalizeEmail().withMessage('Invalid email address'),
+    check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
   ],
   async (req, res) => {
     try {
@@ -21,21 +23,14 @@ router.post(
         return res.status(400).json({ errors: errors.array() });
       }
 
-      const { thoughtText, username, userId } = req.body;
+      const { username, email, password } = req.body;
 
-      // Check if the user exists
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-
-      const thought = await Thought.create({ thoughtText, username });
-
-      // Push the created thought's _id to the associated user's thoughts array field
-      user.thoughts.push(thought._id);
+      // Create a new user
+      const user = await User.create({ username, email });
+      await user.setPassword(password);
       await user.save();
 
-      res.json(thought);
+      res.json({ message: 'User registered successfully' });
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: 'Internal Server Error' });
@@ -43,58 +38,17 @@ router.post(
   }
 );
 
-// POST to create a reaction for a thought with input validation
-router.post(
-  '/thoughts/:thoughtId/reactions',
-  [
-    // Input validation
-    check('reactionBody').isLength({ max: 280 }).withMessage('Reaction must be at most 280 characters'),
-    check('username').trim().notEmpty().withMessage('Username is required'),
-  ],
-  async (req, res) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-
-      const { reactionBody, username } = req.body;
-      const thoughtId = req.params.thoughtId;
-
-      const reaction = { reactionBody, username };
-      const updatedThought = await Thought.findByIdAndUpdate(
-        thoughtId,
-        { $push: { reactions: reaction } },
-        { new: true, runValidators: true }
-      );
-
-      res.json(updatedThought);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  }
-);
-
-// DELETE to remove a reaction by its reactionId value
-router.delete('/thoughts/:thoughtId/reactions/:reactionId', async (req, res) => {
+// POST route for user login
+router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
   try {
-    const thoughtId = req.params.thoughtId;
-    const reactionId = req.params.reactionId;
-
-    const updatedThought = await Thought.findByIdAndUpdate(
-      thoughtId,
-      { $pull: { reactions: { reactionId } } },
-      { new: true }
-    );
-
-    res.json(updatedThought);
+    const token = jwt.sign({ sub: req.user._id }, jwtSecret, { expiresIn: '1h' });
+    res.json({ token });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
+// ... (Your other routes)
+
 module.exports = router;
-
-
