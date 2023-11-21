63734928ecb773f2acb54faaca06cdb7dcbb87ac
Nicolas Esquibel <n.m.esquibel01@gmail.com>
2023-11-20 16:30:59 -0800

    controller

---
+++ models/thoughts.js
@@ -0,0 +1,52 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
+//define reaction
+const ReactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp)
+  }
+});
+
+
+const ThoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp)
+  },
+  username: {
+    type: String,
+    required: true
+  },
+//adds reaction
+reactions: [ReactionSchema]
+},
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false, 
+ }
+);
